b6a7a404b1d0e58b925175713006a504b4e67729
vossiepkv <vossiepkv@gmail.com>
2025-02-18 23:14:03 +1100

    added and styled project page finalised

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,7 @@ function App() {
           />
         </div>
       </div>
+      <p className='location'>Based in Melborune, Victoria, Australia</p>
       <hr/>
 
       {/* Projects */}
@@ -85,12 +86,22 @@ function App() {
 
       <div className='project-container'>
         <div className='project-image-car'>
-          <img src={Car} alt="Car Project"/>
+          <a href='https://24-7-cars.vercel.app/'><img src={Car} alt="Car Project"></img></a>
         </div>
 
         <div className='project-desc-car'>
-          <p className='car-title'>24/7 Cars</p>
+          <a className='car-title' href='https://24-7-cars.vercel.app/'>24/7 Cars</a>
           <p className='car-desc'>This application is a social media community designed for car enthusiasts to connect, share, and engage with others who share their passion for automobiles. Users can showcase their own virtual garage, post pictures and details about their cars, join or create communities based on specific car interests, and organize or discover car meets, events, and shows. The platform fosters interaction through posts, comments, and likes, creating an engaging space for automotive lovers.</p>
+            <div className='tech-badges-car'>
+            <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React" />
+      <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="JavaScript" />
+      <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="Node.js" />
+      <img src="https://img.shields.io/badge/mongodb-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white" alt="MongoDB" />
+      <img src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" alt="Express.js" />
+      <img src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white" alt="AWS" />
+            </div>
+       
+       
         </div>
 
         <hr className='project1-line'/>
@@ -102,17 +113,32 @@ function App() {
         <div className='project-desc-hiro'>
           <p className='hiro-title'>Hiro</p>
           <p className='hiro-desc'>Hiro is a job application platform designed to streamline the job search process by enabling users to mass-apply for jobs effortlessly. The app automates and simplifies job applications, helping users increase their chances of securing employment faster. With Hiro, users can upload their resumes, customise applications, track their job submissions, and receive updates all in one place.</p>
+            <div className='tech-badges-hiro'>
+            <img src="https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React Native" />
+            <img src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase" alt="Firebase" />
+            <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="Node.js" />
+            <img src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" alt="Express.js" />
+            </div>
         </div>
-
-
       </div>
-
+      <div className='git-more'>
+        <a href='https://github.com/vossiepkv' className='git-more'>
+          <img src={Github} alt="GitHub"/>
+          <p>Checkout My Github To See More Projects</p>
+        </a>
+      </div>      
+      
       <hr/>
 
       {/* Education */}
+
+      <div className='edu-header'>
+        <p>Education</p>
+      </div>
 
     </>
   )
 }
 
 export default App
+
